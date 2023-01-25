bbf0b7b4472a9ef49e9da23c96f29008dff53c15
Michele Dorph De Chiffre <md@autonik.dk>
2023-01-25 13:14:37 +0100

    Sposta trasferimento su comp separato

---
+++ src/lib/c/trasferimento.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import AmmontaInput from '$lib/c/ammonta-input.svelte';
+	import BustaSelect from '$lib/c/busta-select.svelte';
+	import type { Trasferimento} from '$lib/stato/trasferimenti';
+	import { createEventDispatcher } from 'svelte';
+
+    export let trasferimento: Trasferimento;
+
+    let inflow: number | undefined = undefined;
+    let outflow: number | undefined = undefined;
+    if (trasferimento.amount > 0) {
+        inflow = trasferimento.amount == 0 ? undefined : trasferimento.amount;
+    } else {
+        outflow = trasferimento.amount == 0 ? undefined : trasferimento.amount * -1;
+    }
+
+    function preventInOutFlow(inorout: 'in' | 'out') {
+        const bothFilled = outflow && inflow;
+        if (inorout == 'in' && bothFilled) {
+            outflow = undefined;
+        } else if (inorout == 'out' && bothFilled) {
+            inflow = undefined;
+        }
+    }
+
+    const dispatch = createEventDispatcher<{salva:{trasferimento: Trasferimento}}>();
+    function salva() {
+        trasferimento.amount = inflow ? inflow : -(outflow || 0);
+        dispatch("salva", {
+            trasferimento
+        });
+    }
+</script>
+
+<form on:submit|preventDefault={salva}>
+    <input name="date" type="date" bind:value={trasferimento.data} placeholder="Data" />
+    <input name="payee" bind:value={trasferimento.payee} placeholder="Payee" />
+    <BustaSelect bind:value={trasferimento.busta}></BustaSelect>
+    <input name="memo" bind:value={trasferimento.memo} placeholder="Memo" />
+    <AmmontaInput name="outflow" bind:value={outflow} placeholder="Outflow" on:blur={() => preventInOutFlow('out')}/>
+    <AmmontaInput name="inflow" bind:value={inflow} placeholder="Inflow" on:blur={() => preventInOutFlow('in')} />
+
+    <button type="submit">Salva</button>
+</form>
--- src/routes/trasferimenti/[contoId]/+page.svelte
+++ src/routes/trasferimenti/[contoId]/+page.svelte
@@ -1,63 +1,43 @@
 <script lang="ts">
     import {page} from '$app/stores'
-	import AmmontaInput from '$lib/c/ammonta-input.svelte';
-	import BustaSelect from '$lib/c/busta-select.svelte';
+	import Trasferimento from '$lib/c/trasferimento.svelte';
 	import { salvaWritable } from '$lib/salvabile';
 	import { getConto } from '$lib/stato/conti';
-	import trasferimentiStato, {type Trasferimento} from '$lib/stato/trasferimenti';
+	import trasferimentiStato, {type Trasferimento as TrasferimentoT} from '$lib/stato/trasferimenti';
 
     const contoId = $page.params.contoId;
     const conto = getConto(contoId);
-    $: trasferimenti = ($trasferimentiStato as Trasferimento[]).filter(v => v.contoId == contoId);
-
-    let trasInEdita: Trasferimento = {
-        id: "",
-        contoId: contoId, /* conto id */
-        payee: "",
-        memo: "",
-        amount: 0,
-        data: toISOstr(new Date()),
-        busta: null, /* busta id */
-        cleared: false
+    $: trasferimenti = ($trasferimentiStato as TrasferimentoT[]).filter(v => v.contoId == contoId);
+    function initialTras() {
+        return {
+            id: "",
+            contoId: contoId, /* conto id */
+            payee: "",
+            memo: "",
+            amount: 0,
+            data: toISOstr(new Date()),
+            busta: null, /* busta id */
+            cleared: false
+        };
     }
-
-    let inflow: number | undefined = undefined;
-    let outflow: number | undefined = undefined;
+    let trasInEdita = initialTras();
 
     function toISOstr(d: Date) {
         return d.toISOString().split('T')[0];
     }
 
-    function preventInOutFlow(inorout: 'in' | 'out') {
-        const bothFilled = outflow && inflow;
-        if (inorout == 'in' && bothFilled) {
-            outflow = undefined;
-        } else if (inorout == 'out' && bothFilled) {
-            inflow = undefined;
-        }
-    }
-
-    function salva() {
-        trasInEdita.amount = inflow ? inflow : -(outflow || 0);
-        salvaWritable(trasInEdita, trasferimentiStato);
+    function salva(event: CustomEvent<{trasferimento:TrasferimentoT}>) {
+        salvaWritable(event.detail.trasferimento, trasferimentiStato);
+        trasInEdita = initialTras();
     }
 </script>
 <h1>Trasferimenti di {conto ? conto.nome : ''}</h1>
 {#if trasferimenti.length < 1}
 <h3>Ingen overførsler</h3>
 {:else}
-    {#each trasferimenti as trasferimento}
-        <p>{trasferimento.data} {trasferimento.amount} {trasferimento.busta} {trasferimento.payee}</p>
+    {#each trasferimenti as trasferimento (trasferimento.id)}
+        <Trasferimento trasferimento={trasferimento} on:salva={salva}></Trasferimento>
     {/each}
 {/if}
 
-<form>
-    <input name="date" type="date" bind:value={trasInEdita.data} placeholder="Data" />
-    <input name="payee" bind:value={trasInEdita.payee} placeholder="Payee" />
-    <BustaSelect bind:value={trasInEdita.busta}></BustaSelect>
-    <input name="memo" bind:value={trasInEdita.memo} placeholder="Memo" />
-    <AmmontaInput name="outflow" bind:value={outflow} placeholder="Outflow" on:blur={() => preventInOutFlow('out')}/>
-    <AmmontaInput name="inflow" bind:value={inflow} placeholder="Inflow" on:blur={() => preventInOutFlow('in')} />
-
-    <button on:click={salva}>Salva</button>
-</form>
+<Trasferimento trasferimento={trasInEdita} on:salva={salva}></Trasferimento>
