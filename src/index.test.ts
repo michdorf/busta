80d9b2069713edcb354e6669c5c8cb690de2b341
Michele Dorph De Chiffre <md@autonik.dk>
2023-01-26 11:59:05 +0100

    Agg. test di ricorrente

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,4 @@
+import Ricorrente from 'moduli/moduli/ricorrente';
 import { describe, it, expect } from 'vitest';
 
 describe('sum test', () => {
@@ -5,3 +6,10 @@ describe('sum test', () => {
 		expect(1 + 2).toBe(3);
 	});
 });
+
+describe('test Ricorrente', () => {
+	it('can repeat each month', () => {
+		let r = new Ricorrente('m',1,new Date(2023,0,4));
+		expect(Ricorrente.prossima(r, new Date(2023,1,4))).toBe(new Date(2023,2,4));
+	})
+})
