5b0b3614bba5be4db3177e7f75cd9275f6423904
Michele Dorph De Chiffre <md@autonik.dk>
2023-01-28 17:44:05 +0100

    Crea e salva automaticamente assegnamenti

--- src/lib/workers/aggiorna-bilanci.ts
+++ src/lib/workers/aggiorna-bilanci.ts
@@ -1,15 +1,16 @@
-import { calcActivity } from "$lib/calc/activity";
 import { monthsDiff, toISOstr } from "$lib/date";
 import appState from "$lib/stato/app-state";
 import Buste, { type BustaT } from "$lib/stato/buste";
 import { get } from "svelte/store";
 
 const aggiornato = get(appState).aggiornato;
+
 if (monthsDiff(new Date(aggiornato), new Date())) {
     console.info("Aggiorna bilanci");
+    debugger;
     
     // Aggiorna precAmonta per ogni busta - ikke relevant, da det kan kalkoleres til en hver tid
-    Buste.update(($buste) => {
+    /* Buste.update(($buste) => {
         $buste = $buste.map(($busta) => {
             let activity = calcActivity($busta);
             $busta.precAmonta = get(activity).corrente + $busta.assegnato;
@@ -18,8 +19,22 @@ if (monthsDiff(new Date(aggiornato), new Date())) {
         });
 
         return $buste;
+    });*/
+
+    // Aggiorna assegnamenti precedenti (se non pronto)
+    Buste.update(($buste) => {
+        return  $buste.map(($busta) => {
+            // let ultimAssegnamento = $busta.assegnamenti[$busta.assegnamenti.length - 1];
+            if (typeof $busta.assegnamenti === "undefined") {
+                $busta.assegnamenti = [];
+            }
+            $busta.assegnamenti.push([aggiornato, $busta.assegnato]);
+            $busta.assegnato = 0;
+            return $busta;
+        });
     });
 
+    // Salva 
     appState.update(($appState) => {
         $appState.aggiornato = toISOstr(new Date());
         return $appState;
