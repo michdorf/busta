c280fcaa60316379525aa89330ca90d822c4e5cd
Michele Dorph De Chiffre <md@autonik.dk>
2023-01-29 12:21:38 +0100

    Migliora/corregge buste calcolazioni

--- src/lib/calc/assegnamenti.ts
+++ src/lib/calc/assegnamenti.ts
@@ -1,12 +1,14 @@
-import { monthsDiff, primoDelMese } from "$lib/date";
-import { roundAmount } from "$lib/numeri";
-import Ricorrente from "moduli/moduli/ricorrente";
-import appState from "$lib/stato/app-state";
 import type { BustaT } from "$lib/stato/buste";
-import { derived, get, readable, type Readable } from "svelte/store";
+import { derived } from "svelte/store";
 import buste from "$lib/stato/buste";
+import { calcActivity } from "./activity";
 
-export function calcAssegnamenti(busta?: BustaT) {
+/**
+ * Nota bene: assegnamenti sono precedenti solo
+ * @param busta 
+ * @returns 
+ */
+export function calcAssegnamentiPrec(busta?: BustaT) {
     return derived(buste, ($buste) => {
         let bs: BustaT[];
         if (typeof busta !== "undefined") {
@@ -23,46 +25,9 @@ export function calcAssegnamenti(busta?: BustaT) {
     });
 }
 
-export function numMesi(busta: BustaT) {
-    return derived(appState, ($appState) => {
-        let finMese: Date = new Date();
-
-        if (busta.target.tipo == 'spending' && 'ripeti' in busta.target) {
-            busta.target.ripeti.primoGiorno = typeof busta.target.ripeti.primoGiorno == "string" ? new Date(busta.target.ripeti.primoGiorno) : busta.target.ripeti.primoGiorno;
-
-            let giornoOffset = new Date($appState.meseSelez.getTime()); // Clona (forse aggiungi a ricorrente TODO)
-            finMese = Ricorrente.prossima(busta.target.ripeti, giornoOffset);
-            console.table({riccorente: busta.target.ripeti, prossima: finMese})
-        } else if ('deadline' in busta.target) {
-            if (busta.target.deadlineAbil) {
-                finMese = new Date(busta.target.deadline);
-            } else {
-                finMese = $appState.meseSelez;
-            }
-        }
-
-        return monthsDiff($appState.meseSelez, finMese)/* + 1*/; // Jeg tæller kun til og ikke med deadline-måneden
-    });
-}
-
-export function calcTargetXMese(busta: BustaT, activity: Readable<{
-    precedente: number;
-    corrente: number;
-    futuro: number;
-}>) {
-    return derived([activity, numMesi(busta)], ([$activity,$numMesi]) => {
-        if (!busta.targetAbilitato) {
-            return 0;
-        }
-    
-        let result = 0;
-    
-        if (busta.target.tipo == 'spending') {
-            result = busta.target.target / $numMesi;
-        } else {
-            result = (busta.target.target - $activity.precedente) / $numMesi;
-        }
-    
-        return roundAmount(result);
+export function calcRolloverAssegnamenti(busta?: BustaT) {
+    let activity = calcActivity(typeof busta != "undefined" ? ($trasferimento) => $trasferimento.busta === busta.id : () => true);
+    return derived([calcAssegnamentiPrec(busta), activity], ([$assegnamenti, $activity]) => {
+        return $assegnamenti - $activity.precedente;
     });
 }
--- src/routes/buste/+page.svelte
+++ src/routes/buste/+page.svelte
@@ -4,18 +4,13 @@
     import Busta from "$lib/c/busta.svelte";
 	import CambiaMese from "$lib/c/cambia-mese.svelte";
 	import { calcActivity } from "$lib/calc/activity";
+	import { calcAssegnamentiPrec, calcRolloverAssegnamenti } from "$lib/calc/assegnamenti";
 	import { primoDelMese } from "$lib/date";
 	import { salvaWritable } from "$lib/salvabile";
 	import appState from "$lib/stato/app-state";
     import Buste, {type BustaT} from "$lib/stato/buste";
-	import categorie from "$lib/stato/categorie";
     import Categorie, { type Categoria } from "$lib/stato/categorie";
 	import Trasferimenti from "$lib/stato/trasferimenti";
-
-    $: conCategoria = $Categorie.map(($categoria) => {
-        return $Buste.filter((busta) => busta.categoria == $categoria.id);
-    });
-    $: senzaCategoria = $Buste.filter((busta) => !busta.categoria); // Assert none
 
     function cambiaCategoriaNome(categoria: Categoria) {
         let nome = prompt("Nuovo nome", categoria.nome);
@@ -39,10 +34,6 @@
         salvaWritable(bustaSelez, Buste);
     }
 
-    let bustaSelez: BustaT;
-    $: balance = $Trasferimenti.reduce((prev, cur) => prev + cur.amount, 0);
-    $: assegnato = $Buste.reduce((prev, cur) => prev + cur.assegnato, 0);
-
     function saldoPrec() {
         let mese = primoDelMese(new Date());
         return $Trasferimenti.reduce((prev, cur) => {
@@ -56,23 +47,32 @@
     function stessoMese(d1: Date, d2: Date) {
         return d1.getMonth() == d2.getMonth() && d1.getFullYear() == d2.getFullYear();
     }
+    
+    $: conCategoria = $Categorie.map(($categoria) => {
+        return $Buste.filter((busta) => busta.categoria == $categoria.id);
+    });
 
-    // TODO: daAssegnare skal være balancen fra forrige måned + alle INDKOMSTER 
+    let bustaSelez: BustaT;
     $: activity = calcActivity();
-    $: mesePrec = $activity.precedente;     
-    $: daAssegnare = mesePrec - assegnato + $Trasferimenti.reduce((prev, cur) => {
+    $: balance = $activity.finora;
+    $: assegnato = $Buste.reduce((prev, cur) => prev + cur.assegnato, 0);
+    // TODO: daAssegnare skal være balancen fra forrige måned + alle INDKOMSTER 
+    $: mesePrec = $activity.precedente;   
+    $: totalRolloverAssegnamenti = calcRolloverAssegnamenti();
+    $: redditoDelMese = $Trasferimenti.reduce((prev, cur) => {
         if (stessoMese(new Date(cur.data), $appState.meseSelez) && (cur.amount > 0)) {
             return prev + cur.amount; 
         } else {
             return prev;
         }
     },0);
+    $: daAssegnare = $totalRolloverAssegnamenti + redditoDelMese - assegnato;
 </script>
 
 <CambiaMese />
 <div>
     <span style="font-size: 2rem;">Da assegnare <Amonta amonta={daAssegnare} /></span><br/>
-    <Amonta amonta={balance} /> balance - <Amonta amonta={assegnato} /> assegnato. <Amonta amonta={mesePrec} /> il mese precedente.
+    <Amonta amonta={balance} /> balance - <Amonta amonta={assegnato} /> assegnato. <Amonta amonta={mesePrec} /> il mese precedente ({$totalRolloverAssegnamenti} rollover).
 </div>
 
 <div class="grid-cont">
