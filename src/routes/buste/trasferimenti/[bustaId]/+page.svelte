cb023aeeeefa95163a5aba40cf0f4ff2d1999445
Michele Dorph De Chiffre <md@autonik.dk>
2023-02-02 10:54:39 +0100

    Implementa trasferimenti della busta (semplice)

--- src/routes/buste/trasferimenti/[bustaId]/+page.svelte
+++ src/routes/buste/trasferimenti/[bustaId]/+page.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
 	import { page } from "$app/stores";
+	import Amonta from "$lib/c/amonta.svelte";
+	import Buste from "$lib/stato/buste";
+	import Trasferimenti from "$lib/stato/trasferimenti";
 
     $: bustaId = $page.params.bustaId;
+    $: trasferimenti = $Trasferimenti.filter(($trasf) => $trasf.busta == bustaId);
 </script>
 
 <h1>{bustaId}</h1>
+
+{#each trasferimenti as trasf}
+<div class="linea">
+    <div>
+        <div style="display: flex;">
+            <div>{trasf.data}</div>
+            <div style="flex: 1; font-weight: bold;">{trasf.payee}</div>
+            <div style="text-align: right;"><Amonta amonta={trasf.amount} /></div>
+        </div>
+        <i>{trasf.memo}</i>
+    </div>
+</div>
+{/each}
+
+<style>
+    .linea > * {
+        padding: 0.3rem;
+    }
+
+    .linea > * > div > div {
+        padding: 0 4px;
+    }
+
+    .linea > *:nth-child(odd) {
+        background-color: rgb(var(--silver));
+    }
+</style>
