f26129c4d02725c6ef90417f172f43e953a5ba17
Michele Dorph De Chiffre <md@autonik.dk>
2023-02-05 15:09:19 +0100

    Risolve login state not registered on first oauth grant

--- src/routes/oauth-callback/+page.svelte
+++ src/routes/oauth-callback/+page.svelte
@@ -3,7 +3,11 @@
 	import { BASEPATH } from '$lib/base-path';
 
     import oauthclient from "$lib/oauth-client";
+	import { initLogin } from '$lib/stato/app-state';
     if (typeof window != "undefined" && window.location.search.indexOf("code=") !== -1) {
-        oauthclient.exchangeAuthCode().then(() => goto(BASEPATH + '/'));
+        oauthclient.exchangeAuthCode().then(() => { 
+            initLogin();
+            goto(BASEPATH + '/');
+        });
     }
 </script>
