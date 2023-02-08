b74d463b4e0e33b924f4d5e47d4a889cc8dbdb80
Michele Dorph De Chiffre <md@autonik.dk>
2023-02-08 11:54:03 +0100

    Risolve ricorrente test failed

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import Ricorrente from 'moduli/moduli/ricorrente';
+import Ricorrente from '$lib/ricorrente';
 import { describe, it, expect } from 'vitest';
 
 describe('sum test', () => {
@@ -7,16 +7,26 @@ describe('sum test', () => {
 	});
 });
 
-describe('test Ricorrente', () => {
+describe('test Ricorrente in mesi', () => {
 	it('can repeat each month', () => {
 		let r = new Ricorrente('m',1,new Date(2023,0,4));
-		expect(Ricorrente.prossima(r, new Date(2023,1,1))).toBe(new Date(2023,2,4));
+		expect(Ricorrente.prossima(r, new Date(2023,1,1))).toStrictEqual(new Date(2023,2,4));
 	})
 })
 
-describe('test Ricorrente', () => {
+describe('test Ricorrente in anni', () => {
     it('can find the previous date', () => {
         let r = new Ricorrente('m',1,new Date(2023,2,4));
-        expect(Ricorrente.scorsa(r)).toBe(new Date(2023,2,4));
+        expect(Ricorrente.scorsa(r)).toStrictEqual(new Date(2023,2,4));
     })
+
+	it('can repeat each year', () => {
+		let r = new Ricorrente('a',1,new Date(2023,0,4));
+		expect(Ricorrente.prossima(r, new Date(2023,1,2))).toStrictEqual(new Date(2024,0,4));
+	})
+
+	it('can repeat 3rd year', () => {
+		let r = new Ricorrente('a',3,new Date(2023,0,4));
+		expect(Ricorrente.prossima(r, new Date(2023,1,2))).toStrictEqual(new Date(2026,0,4));
+	})
 })
