c1eca2f5b034b41f7096e249ad5b00426960429b
Michele Dorph De Chiffre <md@autonik.dk>
2023-02-28 09:04:28 +0100

    Risolve amonta input parse del equazione

--- src/lib/c/ammonta-input.svelte
+++ src/lib/c/ammonta-input.svelte
@@ -7,12 +7,23 @@ export let placeholder = "Estimat"
 
 // ToDo: split in aritmic junks (+ - / * ()) - parse as number and correct comma-sign - join by aritmic characters
 function comma2dot() {
-    let str = `${value}`.replace(/,/g, ".").replace(/[^0-9\.\+\-\/\*\(\)]/g,"");
-    str = str.replace(/(^0+|[\+\-\/*]0+)/,""); // Remove trailing zeros => eval thinks it is not decimal
+    let str = `${value}`;
+    let m = str.match(new RegExp(`[0-9\.,]+`,"g")) // Find alle tal (adskilte af komma eller punktum)
+    if (m !== null) {
+        for (let i = 0; i < m.length; i++) {
+            // Replace, så kun det sidste komma/punktum beholdes (2nd argument)
+            str = str.replace(m[i], m[i].replace(/[\.,]([0-9]+)$/,";$1")
+                                        .replace(/[^0-9;]/g,"")
+                                        .replace(";",".")
+                                        .replace(/^0+([^.].+)/g,"$1") /* remove trailing zeros (not in decimal numbers). eval thinks it is not decimal */);
+        }
+    }
+    str = str.replace(/[^0-9\.\+\-\/\*\(\)]/g,""); // Remove anything other than equation
     let num = eval(str)
     if (isNaN(num)) {
         num = 0;
     }
+
     value = Math.round(num * 100) / 100;
 }
 </script>
