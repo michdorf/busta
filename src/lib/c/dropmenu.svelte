7ad43aecc084a7ba96a82a1324048d8c6777f4a1
Michele Dorph De Chiffre <md@autonik.dk>
2023-03-03 11:03:10 +0100

    Migliora: solo un dropmenu mostra alla volta

--- src/lib/c/dropmenu.svelte
+++ src/lib/c/dropmenu.svelte
@@ -1,5 +1,16 @@
-<div class="dropdown">
-    <button class="dropbtn">...</button>
+<script lang="ts">
+    let show = false;
+    let btn: HTMLButtonElement;
+
+    document.body.addEventListener("click", function (event) {
+        if (event.target !== btn) {
+            show = false;
+        }
+    });
+</script>
+
+<div class="dropdown" class:open={show}>
+    <button class="dropbtn" bind:this={btn} on:click={() => {show = !show}}>...</button>
     <div class="dropdown-content">
         <slot />
     </div>
@@ -13,6 +24,10 @@ font-size: 16px;
 .dropdown {
 position: relative;
 display: inline-block;
+z-index: initial;
+}
+.dropdown.open {
+    z-index: 1;
 }
 
 .dropdown-content {
@@ -20,8 +35,9 @@ display: none;
 position: absolute;
 background-color: #f1f1f1;
 box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-/* z-index: 1; */
 }
 
-.dropdown:hover .dropdown-content {display: block;}
+.dropdown.open .dropdown-content {
+    display: block;
+}
 </style>
